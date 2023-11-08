f904f0f1087065074408a0bb483692011a9dbff3
Chandra Mouli Sappati <csappati@zetaglobal.com>
2023-11-08 23:58:32 +0530

    organised

--- Reactflow-nodes/src/App.jsx
+++ Reactflow-nodes/src/App.jsx
@@ -1,18 +1,11 @@
-import React, { useCallback, useState } from 'react';
-import ELK from 'elkjs';
+import React, { useCallback } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import CardHeader from '@mui/material/CardHeader';
-import Divider from '@mui/material/Divider';
-import ButtonEdge from './components/ButtonEdge';
-
+import ButtonEdge from './components/ButtonEdge/index';
+import CustomNode from './components/CustomNode/index';
+import getLayoutedElements from './components/Layout/index';
 import ReactFlow, {
   useReactFlow,
   ReactFlowProvider,
-  Handle,
-  Position,
   Panel,
   addEdge,
   useEdgesState,
@@ -36,125 +29,6 @@ const edgeTypes = {
   buttonedge: ButtonEdge,
 };
 
-const elk = new ELK();
-
-const getLayoutedElements = async (nodes, edges, options = {}) => {
-  const isHorizontal = options?.['elk.direction'] === 'RIGHT';
-  const graph = {
-    id: 'root',
-    layoutOptions: options,
-    children: nodes.map((node) => ({
-      ...node,
-      targetPosition: isHorizontal ? 'left' : 'top',
-      sourcePosition: isHorizontal ? 'right' : 'bottom',
-
-      width: 150,
-      height: 50,
-    })),
-    edges: edges,
-  };
-
-  try {
-    const layoutedGraph = await elk
-      .layout(graph);
-    return ({
-      nodes: layoutedGraph.children.map((node_1) => ({
-        ...node_1,
-        position: { x: node_1.x, y: node_1.y },
-      })),
-
-      edges: layoutedGraph.edges,
-    });
-  } catch (message) {
-    return console.error(message);
-  }
-};
-
-
-const CustomNode = ({ id }) => {
-
-  const reactFlowInstance = useReactFlow();
-  const nodes = reactFlowInstance.getNodes();
-  const edges = reactFlowInstance.getEdges();
-
-
-  function generateRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-
-  }
-
-  const handleAddButtonClick = (id) => {
-    const nodes = reactFlowInstance.getNodes();
-    const edges = reactFlowInstance.getEdges();
-
-    console.log("nodesssssssss", nodes);
-    console.log("edgessssssssssss", edges);
-
-    const clickNode = nodes.find(node => node.id === id);
-
-    const newNode = {
-      id: uuidv4(),
-      type: 'customNode',
-      position: {
-        x: generateRandomNumber(clickNode.position.x, clickNode.position.x + 1000),
-        y: generateRandomNumber(clickNode.position.y, clickNode.position.y + 1000),
-      },
-      data: {}
-    };
-
-    setNodes(nodes => [...nodes, newNode]);
-
-    const newEdge = {
-      id: uuidv4(),
-      type: 'buttonedge',
-      source: id,
-      target: newNode.id
-    };
-    setEdges(edges => [...edges, newEdge]);
-
-
-  };
-
-  const handleDeleteButtonClick = () => {
-
-    setNodes(nodes => nodes.slice(0, -1));
-    setEdges(edges => edges.slice(0, -1));
-
-  };
-
-
-  const setNodes = (newNodes) => {
-    reactFlowInstance.setNodes(newNodes);
-  };
-
-  const setEdges = (newEdges) => {
-    reactFlowInstance.setEdges(newEdges);
-  };
-
-
-  return (
-    <Card variant='contained' className='card'>
-      <CardHeader className='card-header'
-        title="Node"
-      />
-      <Divider color='#ec6f2c' />
-      <CardContent className='card-content'>
-        <Typography>
-
-          There are three built-in node types  that you can use. {1}
-        </Typography>
-      </CardContent>
-
-      <Handle type="source" position={Position.Right} />
-      <div className='buttons'>
-        <button onClick={() => handleAddButtonClick(id)}>Add</button>
-        <button onClick={handleDeleteButtonClick}>Delete</button>
-      </div>
-      <Handle type="target" position={Position.Left} />
-
-    </Card>
-  );
-};
 
 const nodeTypes = {
   customNode: CustomNode,
@@ -164,7 +38,6 @@ const nodeTypes = {
 const App = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-  // const [number, setNumber] = useState(2);
 
   const { fitView } = useReactFlow();
   console.log('nodes', nodes)
@@ -214,8 +87,6 @@ const App = () => {
 
 
           nodeTypes={nodeTypes}>
-
-
 
 
         </ReactFlow>

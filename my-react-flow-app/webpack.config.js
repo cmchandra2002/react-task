f5491cf811a2d25afd54445316d48b4b05472894
Chandra Mouli Sappati <csappati@zetaglobal.com>
2023-11-08 23:04:42 +0530

    node task added

---
+++ my-react-flow-app/webpack.config.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//     entry: './src/index.js',
+//     output: {
+//         filename: 'bundle.js',
+//         path: path.resolve(__dirname, 'dist'),
+//     },
+//     externals: {
+//         webworker: 'window.Worker',
+//     },
+// };
